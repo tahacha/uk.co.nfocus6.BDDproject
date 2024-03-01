71c1d0f194910e773da30040e13917bf006fa31b
TahaChaudhry <taha.chaudhry@nfocus.co.uk>
2024-03-01 09:51:08 +0000

    Added Hooks class skeleton

---
+++ uk.co.nfocus6.BDDproject/Utils/Hooks.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uk.co.nfocus6.BDDproject.Utils
+{
+    [Binding]
+    public class Hooks
+    {
+        private IWebDriver _driver;
+        private readonly ScenarioContext _scenarioContext;
+
+        public Hooks(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Before]
+        public void SetUp()
+        {
+
+        }
+
+        [After]
+        public void TearDown()
+        {
+
+        }
+
+    }
+
+}
